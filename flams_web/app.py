c98ccddc372190c09b190e9c60d32fa95b567f56
Kasper Oikarinen <kasper@mrfriday.com>
2022-11-28 00:56:40 +0100

    Display Flams results to user in modal and allow user to download results

--- flams_web/app.py
+++ flams_web/app.py
@@ -4,9 +4,11 @@ from flams_web.process import (
     ProcessedRequest,
     process_request,
     run_blast_and_save_result,
+    _get_results_filename
 )
-
+from flask import send_file
 from flams_web.form import InputForm
+import csv
 
 
 app = Flask(__name__)
@@ -26,19 +28,33 @@ def index():
     form = InputForm(modifications=["acetylation"])
     if form.validate_on_submit():
         processed_request = process_request(form)
-        return redirect(
-            url_for("result", processed_request=processed_request.to_json())
-        )
+        if processed_request.is_success:
+            results_file = run_blast_and_save_result(processed_request)
+
+            # Read results from file into an array
+            results=[]
+            with open(results_file) as fd:
+                reader = csv.reader(fd, delimiter="\t")
+                for row in reader:
+                    results.append(row)
+
+            return render_template("index.html", form=form, processed_request=processed_request, results=results)
 
     return render_template("index.html", form=form, processed_request=None)
 
-
 @app.route("/result")
 def result():
-    data = request.args.get("processed_request")
-    processed_request = ProcessedRequest.from_json(data)
-    run_blast_and_save_result(processed_request)
-    return "Success"
+    result_id = request.args.get('id')
+    # Check that the result-id is given AND that it is alphanumeric only
+    # Need to be careful here coz if input is not filtered, user could give path to any file in OS and download it...
+    # Alphanumeric restriction should take care of this.
+    if not result_id or not result_id.isalnum():
+        return "Invalid result ID"
+
+    results_file = _get_results_filename(result_id)
+
+    # Return the results file to the user.
+    return send_file(results_file, as_attachment=True)
 
 
 if __name__ == "__main__":
